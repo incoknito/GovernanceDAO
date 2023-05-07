4c91dfa19b8bf5f117bcc53b106ece4bf50d12a5
Abdulsamad sadiq <otaikisadiq2804@gmail.com>
2023-05-07 20:06:41 +0100

    feat: Add wallet connect feature

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,29 @@
-import './Header.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell, faPlus } from '@fortawesome/free-solid-svg-icons'
-
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import AuthContext from "../../Context/auth-context";
+const shortAddress = (_addr) => _addr.substring(0, 16) + "...";
 const Header = () => {
+  const ctx = useContext(AuthContext);
+  return (
+    <div className="header">
+      <div className="header_nav_buttons">
+        <FontAwesomeIcon className="icon" icon={faBell} />
+        <FontAwesomeIcon className="icon" icon={faPlus} />
+        {!ctx.isConnected && (
+          <button className="connectWallet" onClick={ctx.connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+        {ctx.isConnected && (
+          <button className="addressBar" onClick={ctx.disconnectWallet}>
+            {shortAddress(ctx.account)}
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className='header'>
-            <div className="header_nav_buttons">
-                <FontAwesomeIcon className='icon' icon={faBell} />
-                <FontAwesomeIcon className='icon' icon={faPlus} />
-                <button className='connectWallet'>Connect Wallet</button>
-            </div>
-        </div>
-    )
-}
-
-export default Header
+export default Header;
